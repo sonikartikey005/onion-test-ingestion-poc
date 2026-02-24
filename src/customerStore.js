0c519ac7451985d937d213a2257deef7446895cb
sonikartikey005 <soni.kartikey@gnapitech.com>
2026-02-24 14:57:56 +0530

    Commit to see the action again

--- src/customerStore.js
+++ src/customerStore.js
@@ -3,7 +3,7 @@
 function createCustomerStore() {
 	const customers = new Map(); // id -> customer
 	let nextId = 1;
-	console.log("Testing here 12 3")
+	console.log("Testing here 123")
 
 	function createCustomer({ name, email }) {
 		if (!name) throw new Error("name is required");
